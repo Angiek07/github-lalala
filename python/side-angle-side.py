0fba678ab8f281e02567d10749167bff75d23c18
Angiek07 <63658536+Angiek07@users.noreply.github.com>
2020-05-15 19:56:20 +0200

    - cool summary; - turtle

--- python/side-angle-side.py
+++ python/side-angle-side.py
@@ -1,24 +1,39 @@
 import turtle #don't forget to import also turtle.Screen()
+from math import pi, sin, cos, sqrt
+from numpy import arcsin
 
 def main():
     #describe user what is goin' on
     print("Hi, this programm will compute the measures of your triangle, if you have exactly: side - angel - side.")
+    print("Two sides and the angle between them.")
     print("(Note that the triangle is determined by these three values.)")
     
     #ask user for importrant informations
-    firstSide = input("How long is first of sides? (in mm)")
-    angle = input("How big is your angle? (in °)")
-    secondSide = input ("How long is second side? (also in mm)")
+    firstSide = input("How long is the first side? (in mm)  ")
+    angle = input("How big is your angle? (in °)  ")
+    secondSide = input ("How long is the second side? (also in mm)  ")
 
-    #count it
+    # compute
+    b = float(firstSide)
+    c = float(secondSide)
+    alpha = float(angle) / 180 * pi
+    a = sqrt((b * sin(alpha))**2 + (c - b * cos(alpha))**2)
+    # use sinus theorem
+    sinusBeta = b / a * sin(alpha)
+    if (a**2 + c**2 <= b**2):
+        # angle beta is sharp
+        beta = arcsin(sinusBeta)
+    else:
+        beta = pi - arcsin(sinusBeta)
+    gamma = pi - alpha - beta
 
     #turtle drawing
 
     #finally output
     print(" ")
-    print("Your case:")
+    print("The description of the triangle:")
     something = "something"
-    print("Last side lenght is " + something )
+    print(format("""Last side lenght is " + something )
     print("Angle between your first side and our 'new side' is " + something + "°")
     print("Angle between your second side and our 'new side' is " + something + "°")
     print(" ")
