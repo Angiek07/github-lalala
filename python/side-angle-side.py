13a572074e9a988789223e3bce562ee27d86bfe4
Angiek07 <63658536+Angiek07@users.noreply.github.com>
2020-05-15 11:11:47 +0200

    my first basic
    this is just start....

---
+++ python/side-angle-side.py
@@ -0,0 +1,26 @@
+import turtle #don't forget to import also turtle.Screen()
+
+def main():
+    #describe user what is goin' on
+    print("Hi, this programm will help you with counting rest sides of your triangle, if you have exactly: side - angel - side.")
+    print("We use this smart theorem:")
+    print('"If two pairs of sides of two triangles are equal in length, and the included angles are equal in measurement, then the triangles are congruent."')
+    
+    #ask user for importrant informations
+    firstSide = input("How long is first of sides? (in mm)")
+    angle = input("How big is your angle? (in °)")
+    secondSide = input ("How long is second side? (also in mm)")
+
+    #count it
+
+    #turtle drawing
+
+    #finally output
+    print(" ")
+    print("Your case:")
+    print("Last side lenght is " + #something )
+    print("Angle between your first side and our 'new side' is " + #something + "°")
+    print("Angle between your second side and our 'new side' is " + #something + "°")
+    print(" ")
+    print("You can also look at this basic graph... :)")
+main()
